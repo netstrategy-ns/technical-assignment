6b707abda50827adc844a6174214ec311c5eb86b
Matheus Zimmermann <matheus.zcs@gmail.com>
2026-03-01 11:27:01 +0100

    routes refactored

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@ Route::inertia('/', 'Welcome', [
     'canRegister' => Features::enabled(Features::registration()),
 ])->name('home');
 
-Route::inertia('/cart', 'Cart')
-    ->middleware('auth')
-    ->name('cart');
-
-Route::inertia('/account', 'Account')
-    ->middleware('auth')
-    ->name('account');
-
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
@@ -28,26 +20,22 @@ Route::middleware(['auth', 'verified'])->group(function () {
 Route::get('/events', [EventController::class, 'index'])->name('events.index');
 Route::get('/events/featured', [EventController::class, 'featured'])->name('events.featured');
 Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
-Route::post('/holds', [HoldController::class, 'store'])
-    ->middleware('auth')
-    ->name('holds.store');
-Route::get('/holds', [HoldController::class, 'index'])
-    ->middleware('auth')
-    ->name('holds.index');
-Route::post('/checkout', [CheckoutController::class, 'store'])
-    ->middleware('auth')
-    ->name('checkout.store');
-Route::post('/queue/enter', [EventQueueController::class, 'enter'])
-    ->middleware('auth')
-    ->name('queue.enter');
-Route::get('/queue/status', [EventQueueController::class, 'status'])
-    ->middleware('auth')
-    ->name('queue.status');
 
-Route::middleware('auth')->prefix('account')->group(function () {
-    Route::get('/orders', [AccountOrderController::class, 'index'])->name('account.orders.index');
-    Route::get('/orders/{order}', [AccountOrderController::class, 'show'])->name('account.orders.show');
-    Route::get('/tickets', [AccountTicketController::class, 'index'])->name('account.tickets.index');
+Route::middleware('auth')->group(function () {
+    Route::inertia('/cart', 'Cart')->name('cart');
+    Route::inertia('/account', 'Account')->name('account');
+
+    Route::post('/holds', [HoldController::class, 'store'])->name('holds.store');
+    Route::get('/holds', [HoldController::class, 'index'])->name('holds.index');
+    Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
+    Route::post('/queue/enter', [EventQueueController::class, 'enter'])->name('queue.enter');
+    Route::get('/queue/status', [EventQueueController::class, 'status'])->name('queue.status');
+
+    Route::prefix('account')->group(function () {
+        Route::get('/orders', [AccountOrderController::class, 'index'])->name('account.orders.index');
+        Route::get('/orders/{order}', [AccountOrderController::class, 'show'])->name('account.orders.show');
+        Route::get('/tickets', [AccountTicketController::class, 'index'])->name('account.tickets.index');
+    });
 });
 
 require __DIR__ . '/settings.php';
